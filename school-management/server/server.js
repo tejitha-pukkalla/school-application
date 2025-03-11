7e5be285eadf28ce147994344563f4e0984428e1
tejitha-pukkalla <tejithapukkalla@gmail.com>
2025-03-11 15:46:44 +0530

    CONNECTED DB IN SERVER.JS

--- school-management/server/server.js
+++ school-management/server/server.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
-
 const authRoutes = require("./routes/authroutes");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-connectDB();
+// connectDB();
+const dbConnect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("DB connected");
+    } catch (e) {
+      console.error("Error connecting to MongoDB:", e.message);
+    }
+  };
+  dbConnect();
 
 app.use("/api/auth", authRoutes);
 
